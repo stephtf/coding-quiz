62084ca739fe2ca926cf4397802c386785a39785
Stephanie Fajardo <stfajardo@gmail.com>
2021-12-21 22:34:41 -0500

    added a dope interval timer and a function for subtracting time

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 
-// part 1 - starting the game 
+// variables - starting the quiz 
 
 var body = document.body;
 var startButton = document.getElementById("start-button"); 
@@ -15,9 +15,10 @@ var youAreIncorrect = document.createElement("p");
 youAreIncorrect.textContent = "you are incorrect. try again!";
 
 
-// part 1 a - a disappearing act that results in question 1 
+// part 1 - a disappearing act that results in question One 
 
 function disappear() {
+    setTime();  
     startButton.style.display = "none";  
     startTitle.style.display = "none";
     startPara.style.display = "none";
@@ -31,7 +32,24 @@ function disappear() {
 startButton.addEventListener("click", disappear);
 
 
-// part 2 - when questions are answered 
+// part 2 - setting a timer 
+var secondsLeft = 100;
+var timeEl = document.querySelector(".time");
+
+function setTime() {
+    var timerInterval = setInterval(function() {
+      secondsLeft--;
+      timeEl.textContent = "you have " + secondsLeft + " left";
+  
+      if(secondsLeft === 0) {
+        clearInterval(timerInterval);
+        sendMessage();
+      }
+    }, 1000);
+}
+
+
+// part 3 - when questions are answered (what happens next?!)
 
 function answeredRight() {
     body.appendChild(youAreCorrect);
@@ -39,16 +57,55 @@ function answeredRight() {
 questOneAnswers[2].addEventListener("click", answeredRight);
 
 
+// part 4 - i needed to create a timer to remove the text that gets added when the user answers questions!
+
 function answeredWrong() { 
-    body.appendChild(youAreIncorrect);
-    return;
+    body.appendChild(youAreIncorrect); 
+    youAreIncorrect.style.display="flex";
+    secondsLeft = secondsLeft-10;
+
+    var second = 1
+      function secondTime() {
+        var timerInterval1 = setInterval(function() {
+          second--;
+
+          if(second === 0) {
+            clearInterval(timerInterval1);
+            youAreCorrect.style.display = "none";
+            youAreIncorrect.style.display = "none";
+          }
+        }, 1000);
+      }
+      secondTime();
+} 
+
+function answeredRight() { 
+  body.appendChild(youAreCorrect); 
+  youAreCorrect.style.display="flex";
+  var second = 1
+    function secondTime() {
+      var timerInterval1 = setInterval(function() {
+        second--;
+
+        if(second === 0) {
+          clearInterval(timerInterval1);
+          youAreCorrect.style.display = "none";
+          youAreIncorrect.style.display = "none";
+        }
+      }, 1000);
+    }
+    secondTime();
 }
+
 questOneAnswers[0].addEventListener("click", answeredWrong);
 questOneAnswers[1].addEventListener("click", answeredWrong);
 questOneAnswers[3].addEventListener("click", answeredWrong);
 
-if (answeredRight) {
-    questOne.style.display = "none";
-    questOneAnswers[0].style.display = "none";
-}
+// part 5 - moving on to the next question 
+
+// here i gotta figure out how to move the user to the next question after they answer either correct or incorrectly
+
+
+
+
 
